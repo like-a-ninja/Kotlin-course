3faec0a0c232f57a3717191d20c768fbf1e9ffcd
Tamara Papkova <papkova.td@gmail.com>
2025-10-26 17:44:25 +0300

    homework 14 added with changes

--- src/main/kotlin/lessons/lesson14/Shelf.kt
+++ src/main/kotlin/lessons/lesson14/Shelf.kt
@@ -1,25 +1,28 @@
-package org.popkova.lessons.lesson14
+class Shelf(val capacity: Int) {
+    val items = mutableListOf<String>()
 
-class Shelf(val capacity:Int) {
-    val items= mutableListOf<String>()
-    fun addItem(name:String):Boolean{
-        if (canAccommodate(name)&& !items.contains(name)) {
+    fun addItem(name: String): Boolean {
+        if (canAccommodate(name) && !items.contains(name)) {
             items.add(name)
             return true
-        }else{
+        } else {
             return false
         }
-            }
-    fun removeItem(name:String):Boolean{
-            items.remove(name)
     }
-    fun canAccommodate(name:String):Boolean{
+
+    fun removeItem(name: String): Boolean {
+        return items.remove(name)
+    }
+
+    fun canAccommodate(name: String): Boolean {
         return items.size < capacity
     }
-    fun containsItem (name:String):Boolean{
+
+    fun containsItem(name: String): Boolean {
         return items.contains(name)
     }
+
     fun getItems(): List<String> {
-        return items.toList() // возвращает неизменяемый список
+        return items.toList()
     }
 }
